d78d759b79bd44ead45e726fb695513c72ff0b71
your_username <your_email_address@example.com>
2022-05-23 19:56:35 +0530

    ok '

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './index.js',
+  entry: "./index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'api.bundle.js'
+    path: path.resolve(__dirname, "dist"),
+    filename: "api.bundle.js",
   },
-  target: 'node'
+  target: "node",
+  mode: "development",
 };
